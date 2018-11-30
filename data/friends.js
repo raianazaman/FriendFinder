fdfbc7d33192ca37591d20d8277f2b6faf4f71c5
Raiana Zaman <raiana.salmin@gmail.com>
2018-11-29 19:53:01 -0500

    Add files via upload

--- data/friends.js
+++ data/friends.js
@@ -1,19 +1,25 @@
 
-var friends = [{
-  name: "Ahmed",
-  photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  scores :[
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
-    ]
-},
+var friendMatch = [
+  {
+    name: "Aaron Rodgers",
+    photo: "http://static3.businessinsider.com/image/597a40094528e62c008b4ec6/aaron-rodgers-is-structuring-his-life-like-tom-brady-in-order-to-play-football-into-his-40s.jpg ",
+    scores: ["2", "2", "2", "2", "2"]
+  }, {
+    name: "Aaron Judge",
+    photo: "http://image.nj.com/home/njo-media/width620/img/yankees_main/photo/mlb-baltimore-orioles-at-new-york-yankees-fa5cf650079734ba.jpg ",
+    scores: ["3", "3", "3", "3", "3"]
+  }, {
+    name: " Alex Morgan",
+    photo: "https://i.ytimg.com/vi/LH8RJ9Q0X0M/maxresdefault.jpg ",
+    scores: ["1", "1", "1", "1", "1"]
+  }, {
+    name: "Christen Press",
+    photo: "http://cdn.shopify.com/s/files/1/0661/5963/files/0010_christen_press_large.jpg?9601849381797396010 ",
+    scores: ["3", "3", "3", "3", "1"]
+  },
+
 ];
-module.exports = friends;
+
+
+
+module.exports = friendMatch;
